87cc20650f5c56d1ee79702647390827922e2b33
RafaPach <rafael_eli_pacheco@hotmail.com>
2022-07-15 13:22:00 +0100

    Button fetching

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,22 +3,38 @@ import InputComponent from '../inputComponent/input';
 import './App.css';
 
 function App() {
-  const [city, setCity] = useState("")
+  const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState();
 
-  function onChange(e: React.ChangeEvent<HTMLInputElement>): void{
+  function onChange(e: React.ChangeEvent<HTMLInputElement>): void {
     setCity(e.target.value);
-    console.log(city)
+    console.log(city);
   }
 
-  function Fetching(){
-    // Checks if the city string exists
-    // If it does, send a fetch request
-
+  async function FetchApiData<T>(request: RequestInfo): Promise<T> {
+    // event.preventDefault();
+    const response = await fetch(request);
+    const data = await response.json();
+    setWeatherData(data);
+    return data;
   }
+  let x = () => {
+    try {
+      FetchApiData<{ main: any }>(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cd8d4cf4d94dc362d93ad2e1b9b9adde`
+      ).then(({ main }) => {
+        console.log(main.temp);
+        setWeatherData(main.temp);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="App">
-      <InputComponent handleChange={onChange} city={city}/>
+      <InputComponent handleChange={onChange} city={city} fetching={x} />
+      <p>{weatherData}</p>
     </div>
   );
 }
